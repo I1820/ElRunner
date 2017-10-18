5a27d179dbbe5a6989a3a2d9b018b0eb20aed445
Parham Alvani <parham.alvani@gmail.com>
2017-10-19 00:29:41 +0330

    Adds test for trigger event.

--- runner_test.go
+++ runner_test.go
@@ -18,8 +18,31 @@ func TestIntervalEvent(t *testing.T) {
 			rz <- 1
 		},
 		Interval: 1 * time.Second,
-	})
+	}, 100)
 	go r.Start()
+	<-rz
+	r.Stop()
+}
+
+func TestDataEvent(t *testing.T) {
+	var rz = make(chan int)
+	r := New(&Task{
+		Run: func(e Event) {
+			if e.Type() != DataEventType {
+				t.Fatal("Invalid Event Type")
+			}
+			t.Log(e.Data())
+			rz <- 1
+		},
+		Interval: 0,
+	}, 100)
+	go r.Start()
+	r.Trigger(&DataEvent{
+		data: "Hello",
+	})
+	r.Trigger(&DataEvent{
+		data: "Bye",
+	})
 	<-rz
 	r.Stop()
 }
