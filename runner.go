a94a9b40b0a0a235cddb8bad5f04a4d3ecf20d2b
Parham Alvani <parham.alvani@gmail.com>
2017-10-18 03:34:31 +0330

    Completes runner interface and struct.

--- runner.go
+++ runner.go
@@ -10,34 +10,56 @@
 
 package runner
 
+import "time"
+
 // Runner binded functions
 type Runner interface {
 	Start()
+	Stop()
 }
 
 type runner struct {
-	Task *Task
+	task *Task
+	evs  chan Event
+	stp  chan int
 }
 
 // New creates new runner based on given task
 func New(t *Task) Runner {
-	return &runner{Task: t}
+	return &runner{
+		task: t,
+		evs:  make(chan Event, 100),
+		stp:  make(chan int, 1),
+	}
 }
 
 func (r *runner) listen() {
 	// TODO: Listen for upcomming events
 }
 
-func (r *runner) interval(i int) {
-	// TODO: Listen for interval timeouts
+func (r *runner) interval(i time.Duration) {
+	for {
+		time.Sleep(i)
+		r.evs <- &IntervalEvent{
+			time: time.Now(),
+		}
+	}
+
 }
 
 func (r *runner) Start() {
-	evs := make(chan Event, 100)
 	go r.listen()
-	go r.interval(r.Task.Interval)
+	go r.interval(r.task.Interval)
 	for {
-		ev := <-evs
-		r.Task.Run()
+		select {
+		case ev := <-r.evs:
+			r.task.Run(ev)
+		case <-r.stp:
+			break
+		}
 	}
+}
+
+func (r *runner) Stop() {
+	r.stp <- 1
 }
