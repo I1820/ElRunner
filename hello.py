48600edcb615cb0f2102acdab9d33f13e28e60d1
Parham Alvani <parham.alvani@gmail.com>
2017-11-05 23:54:56 +0330

    Adds hello.py for testing python.

---
+++ hello.py
@@ -0,0 +1,9 @@
+# In The Name Of God
+# ========================================
+# [] File Name : hello.py
+#
+# [] Creation Date : 05-11-2017
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+print("hello from python")
