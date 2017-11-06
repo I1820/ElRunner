33310062130fa97213b17d4e56c90fd3567497d8
Parham Alvani <parham.alvani@gmail.com>
2017-11-06 09:04:30 +0330

    Adds simple and old about message.

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("GoRunner by Parham Alvani")
+
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/api/about", about).Methods("GET")
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	fmt.Printf("Listen on %s\n", server.Addr)
+	server.ListenAndServe()
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "18.20 is leaving us")
 }
