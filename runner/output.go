1ec42e0888e80ace0a76e4aef52d519540217252
Parham Alvani <parham.alvani@gmail.com>
2017-11-09 08:33:34 +0330

    Adds interface for output of the task.

---
+++ runner/output.go
@@ -0,0 +1,15 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 09-11-2017
+ * |
+ * | File Name:     runner/output.go
+ * +===============================================
+ */
+
+package runner
+
+// Output represents output of the task that runs on runner
+type Output interface {
+}
