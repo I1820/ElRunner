40bab64a4dfd718d25c0541fb42e5581fcf1300e
Arash Yadegari <aryadegari@yahoo.com>
2017-11-14 15:19:00 +0330

    complete create and read db operations

---
+++ core/db_crud.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+from core.db_config import db_ip, db_port, db_name, db_collection
+
+client = MongoClient(db_ip, db_port)
+db = client[db_name]
+collection = db[db_collection]
+
+
+def create_one(document):
+    document_id = collection.insert_one(document).inserted_id
+    return document_id
+
+
+def create_many(documents_list):
+    documents_id_list = collection.insert_many(documents_list).inserted_ids
+    return documents_id_list
+
+
+def read_one(partial_document):
+    return collection.find_one(partial_document)
+
+
+def read_many(partial_document):
+    return collection.find(partial_document)
+
+
+def update_one(partial_document, update_instructions):
+    return collection.update_one(partial_document, update_instructions)
---
+++ ui/ui.py
@@ -0,0 +1,64 @@
+import datetime
+
+from core.db_crud import create_one, read_one, create_many, read_many, update_one
+
+test_document = {"ID": "123456789",
+                 "User": "Mike",
+                 "Device ID": "IAU250",
+                 "Message": "device message test 1",
+                 "tags": ["iot", "temperature"],
+                 "date": datetime.datetime.utcnow()}
+
+test_document_2 = {"ID": "1234567890",
+                   "User": "Mike",
+                   "Device ID": "EOA150",
+                   "Message": "device message test 2",
+                   "tags": ["iot", "temperature"],
+                   "date": datetime.datetime.utcnow()}
+
+test_document_3 = {"ID": "12345678900",
+                   "User": "John",
+                   "Device ID": "EOA150",
+                   "Message": "device message test 3",
+                   "tags": ["iot", "temperature"],
+                   "date": datetime.datetime.utcnow()}
+
+
+def create_one_test():
+    print("create_one_test:")
+    document_id = create_one(test_document)
+    print("Created with ID:", document_id)
+
+
+def create_many_test():
+    print("create_many_test:")
+    documents_id_list = create_many([test_document_2, test_document_3])
+    print("Created with IDs:", documents_id_list)
+
+
+def read_one_test():
+    print("read_one_test:")
+    document = read_one({"ID": "123456789"})
+    if document is not None:
+        print("Found:", document)
+    else:
+        print("Nothing Found!")
+
+
+def read_many_test():
+    print("read_many_test:")
+    documents = read_many({"User": "Mike"})
+    print("Documents:")
+    for document in documents:
+        print(document)
+
+
+def update_one_test():
+    print("update_one_test:")
+    result = update_one({"ID": "123456789"}, {"$set": {"Message": "UPDATED device message test 1"}})
+    print(result)
+
+create_one_test()
+create_many_test()
+read_one_test()
+read_many_test()
