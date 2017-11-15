9ab2534069ce2bbf2e0150212c3798c37f81625f
Parham Alvani <parham.alvani@gmail.com>
2017-11-15 16:53:31 +0330

    Adds decode and encode abstract methods.

--- runtime.py/codec.py
+++ runtime.py/codec.py
@@ -7,11 +7,20 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 import pip
+import abc
 
 
-class Codec:
+class Codec(metaclass=abc.ABCMeta):
     @classmethod
     def __init_subclass__(cls, requirements, **kwargs):
         super().__init_subclass__(**kwargs)
         pip.main(['install', *requirements])
         print(cls)
+
+    @abc.abstractmethod
+    def decode(self, data):
+        pass
+
+    @abc.abstractmethod
+    def encode(self, data):
+        pass
