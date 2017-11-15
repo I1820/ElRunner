8ce9a9cc8dc264be1225c716efcf122d3406801e
Parham Alvani <parham.alvani@gmail.com>
2017-11-16 01:05:25 +0330

    Adds decode job.

--- runtime.py/main.py
+++ runtime.py/main.py
@@ -8,17 +8,27 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 import click
+import base64
 from codec import Codec
 
 
 @click.command()
 @click.option('--target', help='Target python source code', type=click.Path())
-def run(target):
-    """ run given target in provided environment """
+@click.option('--job', type=click.Choice(['decode', 'encode', 'rule']))
+def run(target, job):
+    '''
+    run given target in provided environment
+    '''
     try:
-        exec(compile(open(target, "rb").read(), target, 'exec'))
+        exec(compile(open(target, 'r').read(), target, 'exec'))
     except Exception as e:
-        print("Target Error: ", e)
+        print('Target Error: ', e)
+        return
+    if job == 'decode':
+        s = input()
+        s = base64.b64decode(s)
+        d = Codec.get().decode(s)
+        print(d)
 
 
 if __name__ == '__main__':
