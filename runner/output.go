20cf0b52b0c6a8db8a297c2793e42307afa7f7ec
Parham Alvani <parham.alvani@gmail.com>
2017-11-17 13:03:47 +0330

    Removes all output exported fields.

--- runner/output.go
+++ runner/output.go
@@ -10,21 +10,7 @@
 
 package runner
 
-import "fmt"
-
-// Output represents output of the task that runs on runner
-type Output *output
 type output struct {
-	o string
+	s string
 	e int
-}
-
-// CreateOutput creates output from given variable using fmt.Sprint
-// s: output message
-// e: error code (0 = no-error)
-func CreateOutput(v interface{}, e int) Output {
-	return &output{
-		o: fmt.Sprint(v),
-		e: e,
-	}
 }
