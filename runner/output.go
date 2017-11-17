35ab1edeb23c8ff448f63b22523e9908e2895280
Parham Alvani <parham.alvani@gmail.com>
2017-11-17 12:38:17 +0330

    Improves output to support error code.

--- runner/output.go
+++ runner/output.go
@@ -10,5 +10,21 @@
 
 package runner
 
+import "fmt"
+
 // Output represents output of the task that runs on runner
-type Output string
+type Output *output
+type output struct {
+	o string
+	e int
+}
+
+// CreateOutput creates output from given stringer type
+// s: output message
+// e: error code (0 = no-error)
+func CreateOutput(s fmt.Stringer, e int) Output {
+	return &output{
+		o: s.String(),
+		e: e,
+	}
+}
