510cf20d1a082f39574c2c4749356b06a38bcc75
Parham Alvani <parham.alvani@gmail.com>
2017-11-17 13:26:45 +0330

    Adds required comments.

--- codec/event.go
+++ codec/event.go
@@ -15,10 +15,12 @@ import "github.com/aiotrc/GoRunner/runner"
 // DecodeEvent generates for decode call
 type DecodeEvent []byte
 
+// Data returns data associated with event
 func (d DecodeEvent) Data() []byte {
 	return d
 }
 
+// Type returns type of event
 func (d DecodeEvent) Type() int {
 	return runner.UserEventType
 }
@@ -26,10 +28,12 @@ func (d DecodeEvent) Type() int {
 // EncodeEvent generates for encode call
 type EncodeEvent string
 
+// Data returns data associated with event
 func (e EncodeEvent) Data() []byte {
 	return []byte(e)
 }
 
+// Type returns type of event
 func (e EncodeEvent) Type() int {
 	return runner.UserEventType
 }
