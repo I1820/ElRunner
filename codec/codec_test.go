0f863480e4d9c973c846d7b1e97bd1a0fa5f60ee
Parham Alvani <parham.alvani@gmail.com>
2017-11-17 13:26:55 +0330

    Corrects codec tests.

---
+++ codec/codec_test.go
@@ -0,0 +1,35 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 10-11-2017
+ * |
+ * | File Name:     codec/codec_test.go
+ * +===============================================
+ */
+
+package codec
+
+import "testing"
+
+func TestHelloDeocder(t *testing.T) {
+	code := []byte(`
+class ISRC(Codec, requirements=[]):
+    def decode(self, data):
+        return data.decode('ascii')
+    def encode(self, data):
+        pass
+	`)
+	d, err := New(code, "hi")
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, err := d.Decode([]byte("Hi"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r != "Hi\n" {
+		t.Fatal("Invalid Decode Result \"", r, "\"")
+	}
+	d.Stop()
+}
