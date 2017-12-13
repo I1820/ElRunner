06f2600cbbe08895d731fd46f4763bb92c840dfd
Parham Alvani <parham.alvani@gmail.com>
2017-12-13 12:07:54 +0330

    Adds scenario structure basis.

---
+++ scenario/scenario.go
@@ -0,0 +1,18 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 13-12-2017
+ * |
+ * | File Name:     scenario.go
+ * +===============================================
+ */
+
+package scenario
+
+import "github.com/aiotrc/GoRunner/runner"
+
+// Scenario represents rule engine scenario
+type Scenario struct {
+	r runner.Runner
+}
