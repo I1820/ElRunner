02e36c830ee2368230505cdf970c587a8582868c
Parham Alvani <parham.alvani@gmail.com>
2017-12-24 22:27:39 +0330

    Scenario rpc server is ready.

--- scenario/scenario.go
+++ scenario/scenario.go
@@ -10,9 +10,40 @@
 
 package scenario
 
-import "github.com/aiotrc/GoRunner/runner"
+import (
+	"fmt"
+	"net/http"
+	"net/rpc"
+
+	"github.com/aiotrc/GoRunner/runner"
+	"github.com/powerman/rpc-codec/jsonrpc2"
+)
+
+// Endpoint for scenario communication
+type Endpoint struct {
+}
+
+// WaitForData waits for things incomming data
+func (e *Endpoint) WaitForData(args int, reply *string) error {
+	fmt.Println("Helloooooo")
+	*reply = "YesYes"
+	return nil
+}
 
 // Scenario represents rule engine scenario
 type Scenario struct {
-	r runner.Runner
+	r   runner.Runner
+	rpc *rpc.Server
+}
+
+// New creates instance of Scenario
+func New() *Scenario {
+	s := new(Scenario)
+
+	s.rpc = rpc.NewServer()
+	s.rpc.Register(new(Endpoint))
+	h := jsonrpc2.HTTPHandler(s.rpc)
+	go http.ListenAndServe("127.0.0.1:1373", h)
+
+	return s
 }
