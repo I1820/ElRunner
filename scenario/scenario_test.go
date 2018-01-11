9c40f5dd2a95188179441676ab8b91eac9e73f2f
Parham Alvani <parham.alvani@gmail.com>
2018-01-11 20:11:29 +0330

    Using main based testing.

--- scenario/scenario_test.go
+++ scenario/scenario_test.go
@@ -11,18 +11,38 @@
 package scenario
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/powerman/rpc-codec/jsonrpc2"
 )
 
-func TestAbout(t *testing.T) {
-	New()
+func TestMain(m *testing.M) {
+	s := New()
 
+	go func() {
+		err := s.Start()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}()
+
+	os.Exit(m.Run())
+}
+
+func TestAbout(t *testing.T) {
 	var about string
 	c := jsonrpc2.NewHTTPClient("http://127.0.0.1:1373")
-	c.Client.Call("Endpoint.About", nil, &about)
-	if about != "18.20 is leaving us" {
-		t.Fatal("who leaving us?!")
+	err := c.Client.Call("Endpoint.About", nil, &about)
+	if err != nil {
+		t.Fatalf("Call Endpoint.About: %s", err)
 	}
+	if about != "18.20 is leaving us" {
+		t.Fatalf("who leaving us?! %q", about)
+	}
+}
+
+func TestListen(t *testing.T) {
 }
