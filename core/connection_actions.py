d664acfb0dd32e17cee5a1c465e4d95b5b5cb11d
Parham Alvani <parham.alvani@gmail.com>
2018-01-12 02:27:37 +0330

    Converts function into coroutine.

--- core/connection_actions.py
+++ core/connection_actions.py
@@ -1,22 +1,22 @@
 import json
 import sys
-
-import requests
+from requests_threads import AsyncSession
 
 from core.connection_config import server_name, server_port, debug
 
 
 url = 'http://{}:{}/'.format(server_name, server_port)
-headers = {'content-type': 'application/json'}
+session = AsyncSession()
+session.headers = {'content-type': 'application/json'}
 payload = dict(method='method', params=[], jsonrpc='2.0', id=0)
 
 
-def wait_for_data(timeout_seconds):
+async def wait_for_data(timeout_seconds):
     request_payload = payload.copy()
     request_payload['method'] = 'wait_for_data'
     request_payload['params'] = []
     request_payload['id'] = 0
-    response = requests.post(url, data=json.dumps(request_payload), headers=headers, timeout=timeout_seconds).json()
+    response = await session.post(url, data=json.dumps(request_payload), timeout=timeout_seconds).json()
     if response:
         return response
     else:
@@ -25,12 +25,12 @@ def wait_for_data(timeout_seconds):
         return None
 
 
-def send_to_down_link(message, timeout_seconds):
+async def send_to_down_link(message, timeout_seconds):
     request_payload = payload.copy()
     request_payload['method'] = 'send_to_down_link'
     request_payload['params'] = [message]
     request_payload['id'] = 1
-    response = requests.post(url, data=json.dumps(request_payload), headers=headers, timeout=timeout_seconds).json()
+    response = await session.post(url, data=json.dumps(request_payload), timeout=timeout_seconds).json()
     if response:
         return response
     else:
