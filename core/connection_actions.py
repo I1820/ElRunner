5138a53ad98975bbc215e9c16087de989ce77bae
Parham Alvani <parham.alvani@gmail.com>
2018-01-13 01:41:45 +0330

    Corrects connection action asynchronous configuration.

--- core/connection_actions.py
+++ core/connection_actions.py
@@ -1,41 +1,34 @@
-import json
-import sys
-from requests_threads import AsyncSession
+import aiohttp
 
-from core.connection_config import server_name, server_port, debug
+from core.connection_config import server_name, server_port
 
 
 url = 'http://{}:{}/'.format(server_name, server_port)
-session = AsyncSession()
-session.headers = {'content-type': 'application/json'}
-payload = dict(method='method', params=[], jsonrpc='2.0', id=0)
+headers = {'content-type': 'application/json'}
+payload = {'jsonrpc': '2.0'}
 
 
-async def wait_for_data(timeout_seconds):
+async def wait_for_data(timeout):
     request_payload = payload.copy()
-    request_payload['method'] = 'wait_for_data'
+    request_payload['method'] = 'Endpoint.WaitForData'
     request_payload['params'] = []
     request_payload['id'] = 0
-    response = await session.post(url, data=json.dumps(request_payload),
-                                  timeout=timeout_seconds).json()
-    if response:
-        return response
-    else:
-        if debug:
-            print(sys.stderr, 'No Response Received!')
-        return None
+
+    async with aiohttp.ClientSession() as session:
+        session.headers = headers
+        response = await session.post(url, json=request_payload,
+                                      timeout=timeout)
+        return response.json()
 
 
-async def send_to_down_link(message, timeout_seconds):
+async def send_to_down_link(message, timeout):
     request_payload = payload.copy()
-    request_payload['method'] = 'send_to_down_link'
+    request_payload['method'] = 'Endpoint.SendToDownLink'
     request_payload['params'] = [message]
     request_payload['id'] = 1
-    response = await session.post(url, data=json.dumps(request_payload),
-                                  timeout=timeout_seconds).json()
-    if response:
-        return response
-    else:
-        if debug:
-            print(sys.stderr, 'No Response Received!')
-        return None
+
+    async with aiohttp.ClientSession() as session:
+        session.headers = headers
+        response = await session.post(url, json=request_payload,
+                                      timeout=timeout)
+        return response.json()
