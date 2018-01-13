56d04183f2fd30de4e746c76aab647e5f9606847
Parham Alvani <parham.alvani@gmail.com>
2018-01-14 01:16:47 +0330

    Adds function arguments for scheduler.

--- core/time_actions.py
+++ core/time_actions.py
@@ -6,5 +6,5 @@ def sleep(seconds):
     time.sleep(seconds)
 
 
-def schedule(delay_seconds, action_function):
-    Timer(delay_seconds, action_function, ()).start()
+def schedule(delay_seconds, action_function, args=()):
+    Timer(delay_seconds, action_function, args).start()
