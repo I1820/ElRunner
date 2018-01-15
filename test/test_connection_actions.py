213723333af898a7445a22b4c26af1db432a729c
Parham Alvani <parham.alvani@gmail.com>
2018-01-16 00:01:37 +0330

    Corrects connection actions test.

--- test/test_connection_actions.py
+++ test/test_connection_actions.py
@@ -1,32 +1,33 @@
 import threading
-import pytest
 import time
+import pytest
 
 from core import connection_actions
 from core.rpc_server import start_server
 
-server_data_response = 'Test Message'
-server_ack_response = 'OK'
+SERVER_DATA_RESPONSE = 'Test Message'
+SERVER_ACK_RESPONSE = 'OK'
 
 
 def wait_for_data():
-    return server_data_response
+    return SERVER_DATA_RESPONSE
 
 
 def send_to_down_link(message):
-    return server_ack_response
+    print(message)
+    return SERVER_ACK_RESPONSE
 
 
 @pytest.fixture(scope="session")
 def rpc():
-    t = threading.Thread(target=start_server,
-                         daemon=True,
-                         args=(wait_for_data, send_to_down_link))
-    t.start()
+    thread = threading.Thread(target=start_server,
+                              daemon=True,
+                              args=(wait_for_data, send_to_down_link))
+    thread.start()
     # wait for server to load
     time.sleep(2)
 
-    return t
+    return thread
 
 
 @pytest.mark.asyncio
@@ -39,6 +40,6 @@ async def test_wait_for_data(rpc):
 @pytest.mark.asyncio
 async def test_send_to_down_link(rpc):
     response = await connection_actions.send_to_down_link(
-            message=server_data_response, timeout=30)
+        message=SERVER_DATA_RESPONSE, timeout=30)
     print(response)
     assert response['result'] == 'OK'
