3671e8f35e48f1fec7fe123d932098d7fd277243
Parham Alvani <parham.alvani@gmail.com>
2018-01-15 23:53:02 +0330

    Constants in upper case :D

--- core/connection_actions.py
+++ core/connection_actions.py
@@ -3,32 +3,32 @@ import aiohttp
 from core.connection_config import server_name, server_port
 
 
-url = 'http://{}:{}/'.format(server_name, server_port)
-headers = {'content-type': 'application/json'}
-payload = {'jsonrpc': '2.0'}
+URL = 'http://{}:{}/'.format(server_name, server_port)
+HEADERS = {'content-type': 'application/json'}
+PAYLOAD = {'jsonrpc': '2.0'}
 
 
 async def wait_for_data(timeout):
-    request_payload = payload.copy()
+    request_payload = PAYLOAD.copy()
     request_payload['method'] = 'Endpoint.WaitForData'
     request_payload['params'] = []
     request_payload['id'] = 0
 
     async with aiohttp.ClientSession() as session:
-        session.headers = headers
-        response = await session.post(url, json=request_payload,
+        session.headers = HEADERS
+        response = await session.post(URL, json=request_payload,
                                       timeout=timeout)
         return await response.json()
 
 
 async def send_to_down_link(message, timeout):
-    request_payload = payload.copy()
+    request_payload = PAYLOAD.copy()
     request_payload['method'] = 'Endpoint.SendToDownLink'
     request_payload['params'] = [message]
     request_payload['id'] = 1
 
     async with aiohttp.ClientSession() as session:
-        session.headers = headers
-        response = await session.post(url, json=request_payload,
+        session.headers = HEADERS
+        response = await session.post(URL, json=request_payload,
                                       timeout=timeout)
         return await response.json()
