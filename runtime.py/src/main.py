4be1b94030887755d5c1d11740698fad1c3accb7
Parham Alvani <parham.alvani@gmail.com>
2018-01-17 00:33:45 +0330

    New main is here.

--- runtime.py/src/main.py
+++ runtime.py/src/main.py
@@ -11,6 +11,8 @@ import click
 import base64
 import runpy
 
+from codec import Codec
+
 
 @click.command()
 @click.argument('target', type=click.Path())
@@ -20,18 +22,21 @@ def run(target, job):
     run given target in provided environment
     '''
     try:
-        g = runpy.run_path(target)
-        print(g['codecInstance'])
+        g = runpy.run_path(target, run_name='ucodec')
+        for value in g.values():
+            if isinstance(value, type) and issubclass(value, Codec) and \
+                    value.__module__ == 'ucodec':
+                codec = value
     except Exception as e:
         print('Target Error: ', e)
         return
     if job == 'decode':
         s = input()
-        d = g['codec']().decode(base64.b64decode(s))
+        d = codec().decode(base64.b64decode(s))
         print(d)
     if job == 'encode':
         s = input()
-        e = g['codec']().encode(s)
+        e = codec().encode(s)
         print(base64.b64encode(e).decode('ascii'))
 
 
