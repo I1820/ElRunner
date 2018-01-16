9d57c6c8e88e6c47674bec8dea51f15d7e20b070
Parham Alvani <parham.alvani@gmail.com>
2018-01-17 00:32:41 +0330

    New codec is ready for duty.

--- runtime.py/src/codec/codec.py
+++ runtime.py/src/codec/codec.py
@@ -9,21 +9,7 @@
 import abc
 
 
-class CodecBase(abc.ABCMeta):
-    def __init__(self, name, bases, namespace, **kwargs):
-        abc.ABCMeta.__init__(self, name, bases, namespace)
-
-    def __new__(cls, name, bases, namespace):
-        instance = abc.ABCMeta.__new__(
-            cls, name, bases, namespace)
-
-        print(instance)
-        globals()['codecInstance'] = instance
-
-        return instance
-
-
-class Codec(metaclass=CodecBase):
+class Codec(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def decode(self, data):
         pass
