3690e0f7a969b7d672d1a52436f8ddf5d2d608ac
Parham Alvani <parham.alvani@gmail.com>
2018-01-17 00:46:32 +0330

    Adds scenario.

--- runtime.py/src/test/test_time_actions.py
+++ runtime.py/src/test/test_time_actions.py
@@ -1,11 +1,16 @@
 import datetime
+import pytest
+import scenario
 
-from core import time_actions
+
+class TestScenario(scenario.Scenario):
+    def run():
+        pass
 
 
-def test_sleep():
-    time_actions.sleep(seconds=1)
-    time_actions.sleep(seconds=0.1)
+def test_sleep(scenario):
+    scenario.sleep(seconds=1)
+    scenario.sleep(seconds=0.1)
 
 
 def action_function():
@@ -13,6 +18,12 @@ def action_function():
     print(datetime.datetime.now())
 
 
-def test_schedule():
-    time_actions.schedule(delay_seconds=1, action_function=action_function)
-    time_actions.schedule(delay_seconds=0.5, action_function=action_function)
+@pytest.fixture(scope="session")
+def scenario():
+    s = TestScenario()
+    return s
+
+
+def test_schedule(scenario):
+    scenario.schedule(delay_seconds=1, action_function=action_function)
+    scenario.schedule(delay_seconds=0.5, action_function=action_function)
