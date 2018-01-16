fe6e4a4f01eda635a794c92188ba9b59279a158b
Parham Alvani <parham.alvani@gmail.com>
2018-01-16 22:16:38 +0330

    Not me :see_no_evil:.

--- runtime.py/setup.py
+++ runtime.py/setup.py
@@ -45,7 +45,7 @@ setup(
         # For an analysis of "install_requires" vs pip's
         # requirements files see:
         # https://packaging.python.org/en/latest/requirements.html
-        install_requires=['click', 'cbor'],
+        install_requires=reqs,
 
         # To provide executable scripts, use entry points in preference to the
         # "scripts" keyword. Entry points provide cross-platform
