6a3b8217571a6395453b0046668eef5b6a6e168b
Parham Alvani <parham.alvani@gmail.com>
2018-01-17 20:23:55 +0330

    Removes assert usage.

--- runtime.py/src/test/test_analyze.py
+++ runtime.py/src/test/test_analyze.py
@@ -3,5 +3,6 @@ from analyze.pylint_runner import run_pylint, Status
 
 def test_analyze():
     status = run_pylint()
-    assert status in [Status.no_error, Status.convention, Status.warning,
-                      Status.error, Status.fatal]
+    if status not in [Status.no_error, Status.convention, Status.warning,
+                      Status.error, Status.fatal]:
+        raise AssertionError()
