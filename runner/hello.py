be9beba005a664aa5858135113bffc92481cdea0
Parham Alvani <parham.alvani@gmail.com>
2018-01-17 20:11:39 +0330

    Adds module documentation.

--- runner/hello.py
+++ runner/hello.py
@@ -6,10 +6,18 @@
 #
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
+'''
+Hello module just for saying hello from python to go
+'''
 import time
 import base64
 
+# Input
 s = input()
 s = base64.b64decode(s).decode('ascii')
+
+# Thinking ...
 time.sleep(1)
+
+# Output
 print("hello from python", s)
