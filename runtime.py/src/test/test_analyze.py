6bfa49f50268d626a878c5837397b9015da38772
Arash Yadegari <aryadegari@gmail.com>
2018-01-24 12:57:52 +0330

    change analyze config

--- runtime.py/src/test/test_analyze.py
+++ runtime.py/src/test/test_analyze.py
@@ -3,6 +3,5 @@ from analyze.pylint_runner import run_pylint, Status
 
 def test_analyze():
     status = run_pylint()
-    if status not in [Status.no_error, Status.convention, Status.warning,
-                      Status.error, Status.fatal]:
+    if status not in [Status.no_error, Status.error, Status.fatal]:
         raise AssertionError()
