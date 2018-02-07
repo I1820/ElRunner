c77ec4488b96d2d9baabf98f800330630c88d023
Parham Alvani <parham.alvani@gmail.com>
2018-02-07 23:51:04 +0330

    Corrects test_analyze.

--- runtime.py/src/test/test_analyze.py
+++ runtime.py/src/test/test_analyze.py
@@ -2,6 +2,6 @@ from analyze.pylint_runner import run_pylint, Status
 
 
 def test_analyze():
-    status = run_pylint('../scenarios')
+    status = run_pylint('./scenarios')
     if status not in [Status.no_error, Status.error, Status.fatal]:
         raise AssertionError()
