913d8d3760fd91b3460d01f2dee6b8beffc26275
Parham Alvani <parham.alvani@gmail.com>
2018-02-07 23:48:51 +0330

    Adds lint command.

--- runtime.py/src/main.py
+++ runtime.py/src/main.py
@@ -12,15 +12,21 @@ import base64
 import runpy
 
 from codec import Codec
+from analyze import run_pylint_json
 
 
 @click.command()
 @click.argument('target', type=click.Path())
-@click.option('--job', type=click.Choice(['decode', 'encode', 'rule']))
+@click.option('--job', type=click.Choice(['decode', 'encode', 'rule', 'lint']))
 def run(target, job):
     '''
     run given target in provided environment
     '''
+    if job == 'lint':
+        r = run_pylint_json(target)
+        print(r)
+        return
+
     g = runpy.run_path(target, run_name='ucodec')
     for value in g.values():
         if isinstance(value, type) and issubclass(value, Codec) and \
@@ -35,7 +41,6 @@ def run(target, job):
         s = input()
         e = codec().encode(s)
         print(base64.b64encode(e).decode('ascii'))
-    return 0
 
 
 def main():
