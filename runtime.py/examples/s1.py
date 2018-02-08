602c693e4c13897d8dc1893958f5a19ed34cca45
Parham Alvani <parham.alvani@gmail.com>
2018-02-08 22:43:48 +0330

    Adds first scenario example.

---
+++ runtime.py/examples/s1.py
@@ -0,0 +1,22 @@
+from scenario import Scenario
+
+
+class Scenario1(Scenario):
+
+    def __init__(self):
+        self.received = False
+
+    def run(self, data):
+        sender = 'ceitiotlabtest@gmail.com'
+        receivers = ['parham.alvani@gmail.com']
+
+        message = 'From: From Person <ceitiotlabtest@gmail.com>\n' \
+                  'To: To Person <ceitiotlabtest@gmail.com>\n' \
+                  'Subject: Rule Engine Notification\n\n' \
+                  'Data:' + data + '\n' \
+                                   'Sent by Rule Engine. Scenario:1.'
+        self.send_email(host='smtp.gmail.com', port=587,
+                        username="ceitiotlabtest",
+                        password="ceit is the best",
+                        sender=sender,
+                        receivers=receivers, message=message)
