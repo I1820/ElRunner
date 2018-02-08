63b6cda53094d041d4a3e121daafb43aad91387c
Parham Alvani <parham.alvani@gmail.com>
2018-02-08 22:54:49 +0330

    Improves first example.

--- runtime.py/examples/s1.py
+++ runtime.py/examples/s1.py
@@ -1,11 +1,7 @@
 from scenario import Scenario
 
 
-class Scenario1(Scenario):
-
-    def __init__(self):
-        self.received = False
-
+class S1(Scenario):
     def run(self, data):
         sender = 'ceitiotlabtest@gmail.com'
         receivers = ['parham.alvani@gmail.com']
@@ -13,8 +9,8 @@ class Scenario1(Scenario):
         message = 'From: From Person <ceitiotlabtest@gmail.com>\n' \
                   'To: To Person <ceitiotlabtest@gmail.com>\n' \
                   'Subject: Rule Engine Notification\n\n' \
-                  'Data:' + data + '\n' \
-                                   'Sent by Rule Engine. Scenario:1.'
+                  'Data:' + str(data) + '\n' \
+                                        'Sent by Rule Engine. Scenario:1.'
         self.send_email(host='smtp.gmail.com', port=587,
                         username="ceitiotlabtest",
                         password="ceit is the best",
