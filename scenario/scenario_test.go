2296de139589bfc8327c534cbff14da6b04e51b5
Parham Alvani <parham.alvani@gmail.com>
2018-02-09 07:52:09 +0330

    Adds scenario test.

--- scenario/scenario_test.go
+++ scenario/scenario_test.go
@@ -11,8 +11,11 @@
 package scenario
 
 import (
+	"io/ioutil"
 	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/powerman/rpc-codec/jsonrpc2"
 )
@@ -33,5 +36,44 @@ func TestAbout(t *testing.T) {
 	}
 }
 
-func TestListen(t *testing.T) {
+func TestHelloScenario(t *testing.T) {
+	code := []byte(`
+from scenario import Scenario
+
+class ISRC(Scenario):
+    def run(self, data):
+        f = open('/tmp/hello', 'w')
+        f.write(str(data))
+	`)
+
+	s := New()
+
+	h := jsonrpc2.HTTPHandler(s.rpc)
+	srv := httptest.NewServer(h)
+	defer srv.Close()
+
+	if err := s.Code(code, "Hello"); err != nil {
+		t.Fatal(err)
+	}
+	defer s.Stop()
+
+	s.Data("{\"Hello\": 10}")
+
+	if _, err := s.r.OutputBoundedWait(10 * time.Millisecond); err != nil {
+		t.Fatalf("Runs error: %s", err)
+	}
+
+	f, err := os.Open("/tmp/hello")
+	if err != nil {
+		t.Fatalf("Could not open /tmp/hello: %s", err)
+	}
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatalf("Could not read /tmp/hello: %s", err)
+	}
+
+	if string(data) != "{'Hello': 10}" {
+		t.Fatal("%q != {'Hello': 10}", string(data))
+	}
 }
