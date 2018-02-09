7b163223676f061aa796697e95761c1e4e557b5e
Parham Alvani <parham.alvani@gmail.com>
2018-02-09 08:32:17 +0330

    Adds scenario rpc example.

---
+++ runtime.py/examples/s2.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from scenario import Scenario
+
+
+class ISRC(Scenario):
+    def run(self, data):
+        f = open('/tmp/rpc', 'w+')
+        f.write(str(data))
+        f.write('\n')
+
+        loop = asyncio.get_event_loop()
+        t = asyncio.ensure_future(self.wait_for_data(timeout=30))
+        loop.run_until_complete(t)
+        loop.close()
+        response = t.result()
+        if response:
+            f.write('Response:' + str(response))
+        else:
+            f.write('No Response!')
+        f.close()
