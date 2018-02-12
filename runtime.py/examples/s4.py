353811f30691ac68d03314979eec062ffd553bd6
Parham Alvani <parham.alvani@gmail.com>
2018-02-13 01:25:44 +0330

    Adds new example for scenario.

---
+++ runtime.py/examples/s4.py
@@ -0,0 +1,21 @@
+from scenario import Scenario
+from kavenegar import KavenegarAPI
+
+
+class S4(Scenario):
+    def run(self, data):
+        v = self.redis.get("Her")
+        if v is None:
+            self.redis.set("Her", 0)
+        else:
+            self.redis.set("Her", int(v) + 1)
+
+        api = KavenegarAPI('Your APIKey')
+        params = {
+            # optional
+            'sender': '',
+            # multiple mobile number, split by comma
+            'receptor': '09390909540',
+            'message': str(v),
+        }
+        api.sms_send(params)
