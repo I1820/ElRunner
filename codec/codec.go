95f88be2a6caa8eb7fef0c1b0a342f61dd15f56d
Parham Alvani <parham.alvani@gmail.com>
2018-08-03 17:37:00 +0430

    Persists that codecs.

--- codec/codec.go
+++ codec/codec.go
@@ -11,53 +11,83 @@
 package codec
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
-
-	"github.com/aiotrc/GoRunner/runner"
-	log "github.com/sirupsen/logrus"
 )
 
-// Codec provides Encoder/Decoder binded functions
+type command int
+
+const (
+	encodeCommand command = iota
+	decodeCommand command = iota
+)
+
+// Codec provides Encoder/Decode functions
+// based on user codec script
 type Codec struct {
-	r *runner.Runner
-	i string
+	id string
 }
 
 // Decode sends base64 coded data into stdin of
 // user codec script and reads string represntation of json data object
 // from its stdout
-func (c *Codec) Decode(r []byte) (string, error) {
-	c.r.Event(DecodeEvent(base64.StdEncoding.EncodeToString(r)))
-	return c.r.Output()
+func (c *Codec) Decode(ctx context.Context, r []byte) (string, error) {
+	return c.exec(ctx, decodeCommand, base64.StdEncoding.EncodeToString(r))
 }
 
 // Encode sends string represntation of json data object into stdin of
 // user codec script and reads base64 coded data
 // from its stdout
-func (c *Codec) Encode(p string) ([]byte, error) {
-	c.r.Event(EncodeEvent(p))
-	s, e := c.r.Output()
-	if e != nil {
-		return nil, e
+func (c *Codec) Encode(ctx context.Context, p string) ([]byte, error) {
+	s, err := c.exec(ctx, encodeCommand, p)
+	if err != nil {
+		return nil, err
 	}
 	return base64.StdEncoding.DecodeString(s)
 }
 
-// Stop stops codec runner
-func (c *Codec) Stop() {
-	c.r.Stop()
+// exec runs user script for encode/decode
+func (c *Codec) exec(ctx context.Context, t command, input string) (string, error) {
+	var cmd *exec.Cmd
+
+	switch t {
+	case decodeCommand:
+		cmd = exec.CommandContext(ctx, "runtime.py", "--job", "decode", fmt.Sprintf("/tmp/codec-%s.py", c.id))
+	case encodeCommand:
+		cmd = exec.CommandContext(ctx, "runtime.py", "--job", "encode", fmt.Sprintf("/tmp/codec-%s.py", c.id))
+	}
+
+	// stdin
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return "", err
+	}
+	if _, err := io.WriteString(stdin, input); err != nil {
+		return "", err
+	}
+	if err := stdin.Close(); err != nil {
+		return "", err
+	}
+
+	// stdout
+	out, err := cmd.Output()
+	if err != nil {
+		if err, ok := err.(*exec.ExitError); ok {
+			return "", fmt.Errorf("%s: %s", err.Error(), err.Stderr)
+		}
+		return "", err
+	}
+
+	return string(out), nil
+
 }
 
-// ID returns codec identification
-func (c *Codec) ID() string {
-	return c.i
-}
-
-// New creates encoder/decoder based on given data
+// New creates encoder/decoder based on given code
+// it store user script under /tmp/codec-id.py
 func New(code []byte, id string) (*Codec, error) {
 	f, err := os.Create(fmt.Sprintf("/tmp/codec-%s.py", id))
 	if err != nil {
@@ -72,58 +102,19 @@ func New(code []byte, id string) (*Codec, error) {
 		return nil, err
 	}
 
-	runner := runner.New(&runner.Task{
-		Run: func(e runner.Event) (string, error) {
-			var cmd *exec.Cmd
+	return &Codec{
+		id: id,
+	}, nil
+}
 
-			// command
-			switch e.(type) {
-			case DecodeEvent:
-				cmd = exec.Command("runtime.py", "--job", "decode", f.Name())
-			case EncodeEvent:
-				cmd = exec.Command("runtime.py", "--job", "encode", f.Name())
-			default:
-				return "", nil
-			}
-
-			// stdin
-			stdin, err := cmd.StdinPipe()
-			if err != nil {
-				return "", err
-			}
-			if _, err := io.WriteString(stdin, e.Data()); err != nil {
-				return "", err
-			}
-			if err := stdin.Close(); err != nil {
-				return "", err
-			}
-
-			// stdout
-			out, err := cmd.Output()
-			if err != nil {
-				if err, ok := err.(*exec.ExitError); ok {
-					return "", fmt.Errorf("%s: %s", err.Error(), err.Stderr)
-				}
-				return "", err
-			}
-
-			return string(out), nil
-		},
-		Interval: 0,
-	}, 1)
-	runner.ErrHandler = func(err error) {
-		log.WithFields(log.Fields{
-			"code":      id,
-			"job":       "codec",
-			"project":   os.Getenv("NAME"),
-			"component": "gorunner",
-		}).Error(err)
+// NewWithoutCode creates encoder/decoder without any file creation
+// but it check user script existence under /tmp/codec-id.py
+func NewWithoutCode(id string) (*Codec, error) {
+	if _, err := os.Stat(fmt.Sprintf("/tmp/codec-%s.py", id)); err != nil {
+		return nil, err
 	}
 
-	go runner.Start()
-
 	return &Codec{
-		r: runner,
-		i: id,
+		id: id,
 	}, nil
 }
