c277c171b1ee3a7802d5575a7e9ccfcd268226dd
Parham Alvani <parham.alvani@gmail.com>
2018-08-03 19:02:33 +0430

    Adds nodejs project basis.

---
+++ runtime.js/index.js
@@ -0,0 +1,23 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 03-08-2018
+ * |
+ * | File Name:     index.js
+ * +===============================================
+ */
+
+const program = require('commander')
+
+program
+  .version('1.0.0')
+  .option('-t, --target <path>', 'module path')
+  .option('-j, --job <job>', 'job type', /^(decode|encode|rule)$/i)
+  .parse(process.argv)
+
+console.log(program.job)
+console.log(program.target)
