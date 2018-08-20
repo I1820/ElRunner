9b2388ddec3a268e33320b1f29b5d65a979af09c
Parham Alvani <parham.alvani@gmail.com>
2018-08-21 02:20:28 +0430

    Adds create and list codec apis.

---
+++ actions/codec.go
@@ -0,0 +1,81 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 21-08-2018
+ * |
+ * | File Name:     codec.go
+ * +===============================================
+ */
+
+package actions
+
+import (
+	"io/ioutil"
+	"net/http"
+	"regexp"
+
+	"github.com/I1820/ElRunner/codec"
+	"github.com/gobuffalo/buffalo"
+)
+
+// scenario, codec request payload
+type codeReq struct {
+	ID   string `json:"id" binding:"required"`
+	Code string `json:"code" binding:"required"`
+}
+
+// CodecsResource manages existing codecs
+type CodecsResource struct {
+	buffalo.Resource
+}
+
+var codecRegexp *regexp.Regexp
+
+func init() {
+	rg, err := regexp.Compile(`codec-(\w*).py`)
+	if err == nil {
+		codecRegexp = rg
+	}
+}
+
+// List lists available codecs. This function is mapped
+// to the path GET /codecs
+func (CodecsResource) List(c buffalo.Context) error {
+	codecs := make([]string, 0)
+
+	files, err := ioutil.ReadDir("/tmp")
+	if err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	for _, f := range files {
+		name := f.Name()
+		if s := codecRegexp.FindStringSubmatch(name); len(s) > 0 && s[0] == name {
+			codecs = append(codecs, s[1])
+		}
+	}
+
+	return c.Render(http.StatusOK, r.JSON(codecs))
+}
+
+// Create creates new codec and stores it code. This function is mapped
+// to the path POST /codecs
+func (CodecsResource) Create(c buffalo.Context) error {
+	var rq codeReq
+	if err := c.Bind(&rq); err != nil {
+		return c.Error(http.StatusBadRequest, err)
+	}
+
+	id := rq.ID
+
+	_, err := codec.New([]byte(rq.Code), id)
+	if err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+
+	return c.Render(http.StatusOK, r.JSON(id))
+}
