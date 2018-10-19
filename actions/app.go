cd8d2e205037eea26389dbe137c6b0dc95aa61db
Parham Alvani <parham.alvani@gmail.com>
2018-10-19 23:24:54 +0330

    Removes ssl configuration.

--- actions/app.go
+++ actions/app.go
@@ -6,10 +6,8 @@ import (
 	linkapp "github.com/I1820/ElRunner/app"
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/buffalo/middleware"
-	"github.com/gobuffalo/buffalo/middleware/ssl"
 	"github.com/gobuffalo/envy"
 	"github.com/sirupsen/logrus"
-	"github.com/unrolled/secure"
 	"github.com/weekface/mgorus"
 
 	"github.com/gobuffalo/x/sessions"
@@ -35,27 +33,10 @@ func App() *buffalo.App {
 			},
 			SessionName: "_el_runner_session",
 		})
-		// Automatically redirect to SSL
-		app.Use(ssl.ForceSSL(secure.Options{
-			SSLRedirect:     ENV == "production",
-			SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
-		}))
-
-		// Set the request content type to JSON
-		app.Use(middleware.SetContentType("application/json"))
-		app.Use(func(next buffalo.Handler) buffalo.Handler {
-			return func(c buffalo.Context) error {
-				defer func() {
-					c.Response().Header().Set("Content-Type", "application/json")
-				}()
-
-				return next(c)
-			}
-		})
 
 		// Create mongodb connection
 		url := envy.Get("DB_URL", "mongodb://172.18.0.1:27017")
-		hooker, err := mgorus.NewHooker(url, "i1820", fmt.Sprintf("projects.logs.%s", envy.Get("NAME", "ElRunner")))
+		hooker, err := mgorus.NewHooker(url, "i1820", fmt.Sprintf("projects.logs.%s", envy.Get("PROJECT", "ElRunner")))
 		if err == nil {
 			logrus.AddHook(hooker)
 			logrus.Infof("Logrus MongoDB Hook is %s", url)
