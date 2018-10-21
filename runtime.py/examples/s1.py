97c22cee9e42e9079968fa504501f4c62a55cdfc
Parham Alvani <parham.alvani@gmail.com>
2018-10-21 13:37:54 +0330

    Brand new mail scenario.

--- runtime.py/examples/s1.py
+++ runtime.py/examples/s1.py
@@ -2,17 +2,17 @@ from scenario import Scenario
 
 
 class S1(Scenario):
-    def run(self, data=None):
-        sender = 'ceitiotlabtest@gmail.com'
+    async def run(self, data=None):
+        sender = 'platform.avidnetco@gmail.com'
         receivers = ['parham.alvani@gmail.com']
 
-        message = 'From: From AIoTRC <ceitiotlabtest@gmail.com>\n' \
+        message = 'From: From Avidnet <platform.avidnetco@gmail.com>\n' \
                   'To: To Parham Alvani <parham.alvani@gmail.com>\n' \
                   'Subject: Rule Engine Notification [Thing: ' + self.id + ']\n\n' \
                   'Data:' + str(data) + '\n' \
-                                        'Sent by Rule Engine. Scenario:1.'
+                                        'Sent by Rule Engine. Scenario-1.'
         self.send_email(host='smtp.gmail.com', port=587,
-                        username="ceitiotlabtest",
-                        password="ceit is the best",
+                        username="platform.avidnetco@gmail.com",
+                        password="fancopass(1397)",
                         sender=sender,
                         receivers=receivers, message=message)
